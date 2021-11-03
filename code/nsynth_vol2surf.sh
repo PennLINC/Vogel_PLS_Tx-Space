ae01ed211eec4bae8f25cad5c243af9b960bee7b
Jacob Vogel <illdopejake@gmail.com>
2021-11-03 17:51:50 -0400

    adding nsynth vol2surf script

---
+++ code/nsynth_vol2surf.sh
@@ -0,0 +1,4 @@
+## NOTE: Change the first command so that it reflects the path or alias for your instance wb_command
+
+/Users/jacobv/Downloads/workbench/bin_macosx64/wb_command -volume-to-surface-mapping ../neurosynth_topics/tmp.nii.gz ../surf/S1200.L.midthickness_MSMAll.32k_fs_LR.surf.gii ../neurosynth_topics/tmp.func.gii -enclosing
+
